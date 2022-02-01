fc614024491b986b9f6438eb40a7b2beb9a11010
erik thalen <erik.thalen@hyperisland.se>
2022-02-01 23:37:03 +0100

    added ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,11 +1,11 @@
 import geoip from 'fast-geoip'
+import { IncomingMessage } from 'http'
 
 const getCountryName = new Intl.DisplayNames(['en'], { type: 'region' })
 
-export const makeRecord = async req => {
-  const ip =
-    (req.headers['x-forwarded-for'] || '').split(',')[0] ||
-    req.connection.remoteAddress
+export const makeRecord = async (req: IncomingMessage) => {
+  const forwardedForHeaders = Array.isArray(req.headers['x-forwarded-for']) ? req.headers['x-forwarded-for'][0] : req.headers['x-forwarded-for'];
+  const ip = forwardedForHeaders.split(',')[0] || req.socket.remoteAddress
 
   if (!ip || ip === '::ffff:127.0.0.1') return
 
