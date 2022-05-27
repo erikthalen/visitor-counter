58abfb064278a5b47315bb5f27c28a1c20af2b33
Erik Thalén <erik.thalen@hyperisland.se>
2022-05-27 23:23:10 +0200

    feat(db): change name of db collection

--- demo/faker.mjs
+++ demo/faker.mjs
@@ -1,12 +1,11 @@
-import geoip from 'fast-geoip'
 import { db } from '../lib/db.mjs'
+import { createModel } from '../lib/api.mjs'
 
 const zero255 = () => Math.floor(Math.random() * 255)
 const randomIP = () => `${zero255()}.${zero255()}.${zero255()}.${zero255()}`
-const getCountryName = new Intl.DisplayNames(['en'], { type: 'region' })
 
 const database = await db({
-  mongourl: 'mongodb://localhost:27017/',
+  mongourl: 'mongodb://127.0.0.1:27017',
   id: 'default',
 })
 
@@ -16,25 +15,20 @@ const fakeIt = () => {
   return Promise.all(
     [...Array(COUNT)].map(async (_, i) => {
       const ip = randomIP()
-      const location = await geoip.lookup(ip)
+
+      await database.set(
+        await createModel(ip, Math.floor(Date.now() - Math.random() * 10000000000))
+      )
 
       console.clear()
       console.log(`Created ${i} fake users`)
-
-      await database.set({
-        ip,
-        countryCode: location?.country || false,
-        country: location ? getCountryName.of(location.country) : false,
-        date: Math.floor(Date.now() - Math.random() * 10000000000),
-      })
     })
   )
 }
 
 try {
   await fakeIt()
-  await fakeIt()
-  console.log(`Done creating ${COUNT * 2} fake users`)
+  console.log(`Done creating ${COUNT} fake users`)
 } catch (error) {
   console.log('Error creating fake visitors:', error)
 }
